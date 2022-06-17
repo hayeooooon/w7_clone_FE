85f7f062da8f979f9ae7088e415fb937db8ce568
xira94 <guswnschl45@gmail.com>
2022-06-17 16:15:38 +0900

    로그인 해봤수다

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import './App.css';
-import {Routes, Route} from 'react-router-dom';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
-import Main from './components/Main';
+import Main from "./components/Main";
+import Register from "./components/Register";
 
 function App() {
   return (
-     <div className="App" style={{width: '100%', maxWidth: '500px', minHeight: '100vh', margin: '0 auto', border: '1px solid #ddd'}}> {/* 최대 가로 사이즈 500 고정, 가운데 정렬하는 style */}
+    <div
+      className="App"
+      style={{
+        width: "100%",
+        maxWidth: "500px",
+        minHeight: "100vh",
+        margin: "0 auto",
+        border: "1px solid #ddd",
+      }}
+    >
+      {" "}
+      {/* 최대 가로 사이즈 500 고정, 가운데 정렬하는 style */}
       <Routes>
-      <Route path="/" element={<Main/>}></Route>
+        <Route path="/" element={<Main />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
---
+++ src/components/Register.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../css/Register.css";
+
+const Register = () => {
+  return (
+    <div className="regi_main">
+      <h1>MUNTO</h1>
+      <div className="regi_mid">
+        <h5>이메일</h5>
+        <input type="text" placeholder="이메일을 입력해 주세요." />
+        <h5>비밀번호</h5>
+        <input type="text" placeholder="비밀번호를 입력해 주세요." />
+      </div>
+      <div className="regi_btn">
+        <button>로그인</button>
+      </div>
+      <div className="regi_bottom">
+        <u>계정정보를 잊으셨나요?</u>
+        <a>회원가입하기</a>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
