f46d696345e3c11d8539424f5cfe2b3dccceabc9
xira94 <guswnschl45@gmail.com>
2022-06-17 20:52:46 +0900

    220617 20:52 Login & Regi Pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
 import Main from "./components/Main";
+import Login from "./components/Login";
 import Register from "./components/Register";
+import RegiProfile from "./components/RegiProfile";
+import Interest from "./components/Interest";
 
 function App() {
   return (
@@ -16,11 +19,13 @@ function App() {
         border: "1px solid #ddd",
       }}
     >
-      {" "}
       {/* 최대 가로 사이즈 500 고정, 가운데 정렬하는 style */}
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/regiProfile" element={<RegiProfile />} />
+        <Route path="/interest" element={<Interest />} />
       </Routes>
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,12 @@
 import React from "react";
-import "../css/Login.css";
 import { useNavigate } from "react-router-dom";
-const Register = () => {
-  const nagivate = useNavigate();
+import "../css/Login.css";
 
-  const onClickNext = () => {
-    nagivate("/regiprofile");
+const Login = () => {
+  const navigate = useNavigate();
+
+  const onClickRegi = () => {
+    navigate("/register");
   };
   return (
     <div className="regi_main">
@@ -13,18 +14,18 @@ const Register = () => {
       <div className="regi_mid">
         <h5>이메일</h5>
         <input type="text" placeholder="이메일을 입력해 주세요." />
-        <h5>닉네임</h5>
-        <input type="text" placeholder="닉네입을 입력해 주세요." />
         <h5>비밀번호</h5>
         <input type="text" placeholder="비밀번호를 입력해 주세요." />
-        <h5>비밀번호 확인</h5>
-        <input type="text" placeholder="비밀번호 확인." />
       </div>
       <div className="regi_btn">
-        <button onClick={onClickNext}>다음</button>
+        <button>로그인</button>
+      </div>
+      <div className="regi_bottom">
+        <u>계정정보를 잊으셨나요?</u>
+        <a onClick={onClickRegi}>회원가입하기</a>
       </div>
     </div>
   );
 };
 
-export default Register;
+export default Login;
